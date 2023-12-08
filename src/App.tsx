b5ee7503ec71a40e4a7e8498d11ce70706a2acc2
MohammedNawazMynulIslam <mynulislam183@gmail.com>
2023-12-08 09:49:58 +0600

    added shadcn/ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
+import { Home } from './Pages/Home/Home';
+import { MainLayout } from './Pages/Home/MainLayout';
+
+import { SignIn } from './SignIn/SignIn';
+import { SignUp } from './SignUp/SignUp';
 import './global.css'
 import {Routes, Route} from 'react-router-dom';
 const App = () => {
     return (
         <main className='flex h-screen'>
             <Routes>
-                <Route path="/signIn" element={<SignIn />}/>
-                <Route path="/" element={<HomePage />}/>
 
+                <Route path="/signIn" element={<SignIn />}/>
+                <Route path="/signUp" element={<SignUp />}/>
+                <Route element={<MainLayout></MainLayout>}>
+                <Route path="/" element={<Home />}/>
+                </Route>
             </Routes>
         </main>
     );
